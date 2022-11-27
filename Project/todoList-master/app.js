0f2d74af0112d46944e9acd80bc8c99c9dda0916
Sonu1508 <95277583+Sonu1508@users.noreply.github.com>
2022-11-27 10:33:51 +0530

    Update app.js

--- Project/todoList-master/app.js
+++ Project/todoList-master/app.js
@@ -3,12 +3,12 @@ let text = addForm.add;
 let ul = document.querySelector(".todos");
 
 ul.addEventListener("click",(e) => {
-	if(e.target.nodeName === "I")	// click hone pe kya click hua hai li pe span,blankarea(li),trashIcon pe I aayega e.target.nodeName se
-		e.target.parentElement.remove();	//parent ul se jo target hoga jo click kiya hoga wo haat jayega
+	if(e.target.nodeName === "I")	
+		e.target.parentElement.remove();	
 });
 let addItem = (item) =>{
 
-	// we can add element with help of appendchild as well but we want li with decoration i mean we have to set lots of attributes that's why we're using this trick
+	
 	let str = `<li class="list-group-item d-flex justify-content-between align-items-center">
 				<span>${item}</span>
 				<i class="far fa-trash-alt delete"></i>
@@ -19,9 +19,9 @@ let addItem = (item) =>{
 
 addForm.addEventListener("submit",(e) => {
 	
-	e.preventDefault();		// canceling the default behaviour of form
-	let item = text.value;	// we will get whatever user will enter as there todo list
-	if(item.length > 0)		// empty row ko add nhi hone den
+	e.preventDefault();		
+	let item = text.value;	
+	if(item.length > 0)		
 	addItem(item);
 	text.value = "";	//for text box erase
 
@@ -32,14 +32,14 @@ let searchItem = (text) => {
 	let listitems = ul.children;
 	for(let li of listitems)
 	{
-		if(li.innerText.toLowerCase().indexOf(text)===-1)	//equalsIgnoreCase() bhi laga skate hai thys
+		if(li.innerText.toLowerCase().indexOf(text)===-1)	
 			li.classList.add("filtered");
 		else
 			li.classList.remove("filtered");
 	}
 }
 
-let serachText = document.querySelector(".search input");	// serach parent class me input dundo
+let serachText = document.querySelector(".search input");	
 serachText.addEventListener("keyup",(e) =>{
 	searchItem(serachText.value.toLowerCase());
 });
